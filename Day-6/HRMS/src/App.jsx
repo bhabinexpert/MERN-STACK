aa703bd8a3b622505fa8f2bd43f9b6d9239dc987
Bhabin Dulal <bhabindulal35@gmail.com>
2025-07-03 14:18:29 +0545

    rendered home page and redirect to another page from home

--- Day-6/HRMS/src/App.jsx
+++ Day-6/HRMS/src/App.jsx
@@ -4,6 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import LoginForm from './pages/login'
+import Home from './pages/home'
+import Employee from './pages/employee'
+
 
 function App() {
 
@@ -11,10 +14,13 @@ function App() {
     <>
     <Routes>
       <Route path = '/' element = {<LoginForm/>}></Route>
+      <Route path = 'home' element = {<Home/>}>
+        <Route path = 'employee' element = {<Employee/>}> </Route>
+      </Route>
     </Routes>
       
     </>
-  )
+  ) 
 }
 
 export default App
---
+++ Day-6/HRMS/src/pages/home.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import TopBar from "../components/topbar";
+import Sidebar from "../components/home/siderbar";
+
+export default function Home(){
+    return(
+        <div className = " flex flex-col h-screen">
+            <TopBar/>
+            <div className=" flex flex-1 overflow-hidden">
+                <div>
+                    <Sidebar/>
+                </div>
+                <div className="flex-1 overflow-y-auto p-5">
+                    <Outlet/>
+                </div>
+            </div>
+        </div>
+
+    )
+}
