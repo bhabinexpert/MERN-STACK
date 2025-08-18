c70f95e4d4c6a8c1c9ea173482fc8daa7050d021
Bhabin Dulal <bhabindulal35@gmail.com>
2025-08-18 09:44:20 +0545

    native setup

---
+++ Day-6/MobileApp/app/index.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "../global.css"
+
+export default function Index() {
+  return (
+    <View
+     
+    >
+      <Text> Hello World</Text>
+    </View>
+  );
+}
