c929e85cd0a0e78a5dd139a735f91379bd4c5461
Bhabin Dulal <bhabindulal35@gmail.com>
2025-08-19 09:59:58 +0545

    login form created

---
+++ Day-6/MobileApp/app/auth/login.jsx
@@ -0,0 +1,84 @@
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import axios from 'axios'
+
+
+const login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password:""
+  })
+
+  const [error, setError] = useState("")
+
+  
+  const handleLogin = async ()=>{
+    
+    if(!formData.email || !formData.password){
+      setError("All fields are required!")
+      return;
+    }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(formData.email)) {
+    setError("Invalid Email!")
+    return ;
+  }
+  try {
+    const response = await axios.post("http://localhost:9000/auth",formData)
+    console.log(response)
+
+    if(response.status === 200){
+      console.log(response.data)
+      
+      // const token = response.data.token;
+      // const user = response.data.user;
+
+    }
+    
+  } catch (error) {
+    setError( error.response.data.message)
+    
+  }
+    
+
+  }
+
+  
+  return (
+    <View className = "flex-1 flex-col items-center gap-4 px-6" >
+      <Text className = "text-xl font-bold" >Welcome back!</Text>
+      <View className = "flex flex-col gap-3 w-full" >
+        <Text className = 'text-lg'> Email</Text>
+        <TextInput className ="border p-2 rounded-md"
+        placeholder='Enter your email'
+        autoCapitalize={false}
+        keyboardType='email-address'
+        onChangeText={(text)=> setFormData((prev)=>({...prev, email: text}))} ></TextInput>
+      
+
+        <Text className = 'text-lg'> Password</Text>
+        <TextInput className ="border p-2 rounded-md"
+        placeholder='********' 
+        autoCapitalize= {false}
+        secureTextEntry = {true}
+         onChangeText={(text)=> setFormData((prev)=>({...prev, password: text}))}
+
+        ></TextInput>
+
+        {error && <Text className = 'text-red-500'> {error}</Text>}
+
+      </View>
+
+      {/* Login button */}
+
+      <TouchableOpacity
+      onPress={handleLogin}
+      className = "bg-black w-full items-center p-2 rounded-md">
+        <Text className ="text-white"> Login</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default login 
--- Day-6/MobileApp/app/index.jsx
+++ Day-6/MobileApp/app/index.jsx
@@ -1,11 +1,16 @@
 import { Text, View } from "react-native";
 import "../global.css"
+import { Redirect } from "expo-router";
 
 export default function Index() {
+  const isLoggedIn = false;
+
+  if (!isLoggedIn){
+    return <Redirect href = '/auth/login' />;
+  }
+  
   return (
-    <View
-     
-    >
+    <View>
       <Text> Hello World</Text>
     </View>
   );
